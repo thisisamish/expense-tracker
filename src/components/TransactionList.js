70ed40b210b06dca7a0685b5cd07554be658766b
Amish Verma <amishhverma@gmail.com>
2022-11-28 13:15:59 +0530

    Create context and use it in TransactionList component

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,14 +1,20 @@
 import Transaction from "./Transaction";
+import { GlobalContext } from "../context/GlobalState";
+import { useContext } from "react";
 
 const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
   return (
     <>
       <h3>History</h3>
       <ul id="list" className="list">
-        <li className="minus">
-          Cash <span>-$400</span>
-          <button className="delete-btn">x</button>
-        </li>
+        {transactions.map((transaction) => (
+          <li className="minus" key={transaction.id}>
+            {transaction.text} <span>-$400</span>
+            <button className="delete-btn">x</button>
+          </li>
+        ))}
       </ul>
     </>
   );
